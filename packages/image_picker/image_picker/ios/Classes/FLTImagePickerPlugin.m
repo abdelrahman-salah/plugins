4e5cb304c0fae4c6101891107f2c9fdda424f181
abdelrahman-salah <36513217+abdelrahman-salah@users.noreply.github.com>
2020-10-02 06:08:37 +0200

    Update FLTImagePickerPlugin.m
    fixed typo

--- packages/image_picker/image_picker/ios/Classes/FLTImagePickerPlugin.m
+++ packages/image_picker/image_picker/ios/Classes/FLTImagePickerPlugin.m
@@ -69,7 +69,7 @@ static const int SOURCE_GALLERY = 1;
   if ([@"pickImage" isEqualToString:call.method]) {
     _imagePickerController = [[UIImagePickerController alloc] init];
     _imagePickerController.modalPresentationStyle = UIModalPresentationPopover;
-    _imagePickerController.isModalInPresentation = true;
+    _imagePickerController.modalInPresentation = true;
     _imagePickerController.delegate = self;
     _imagePickerController.mediaTypes = @[ (NSString *)kUTTypeImage ];
 
