473fbf8941f644b79ba693990fd90c89d712bd5d
abdelrahman-salah <36513217+abdelrahman-salah@users.noreply.github.com>
2020-10-02 05:40:30 +0200

    Update FLTImagePickerPlugin.m
    hot fix for https://github.com/flutter/flutter/issues/66647

--- packages/image_picker/image_picker/ios/Classes/FLTImagePickerPlugin.m
+++ packages/image_picker/image_picker/ios/Classes/FLTImagePickerPlugin.m
@@ -68,7 +68,8 @@ static const int SOURCE_GALLERY = 1;
 
   if ([@"pickImage" isEqualToString:call.method]) {
     _imagePickerController = [[UIImagePickerController alloc] init];
-    _imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
+    _imagePickerController.modalPresentationStyle = UIModalPresentationPopover;
+    _imagePickerController.isModalInPresentation = true;
     _imagePickerController.delegate = self;
     _imagePickerController.mediaTypes = @[ (NSString *)kUTTypeImage ];
 
